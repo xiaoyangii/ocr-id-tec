e7cc9264937879e5e58d490e2bd8771217be67ad
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-03 18:46:43 +0800

    publicPath

--- ocr-id-tec/vue.config.js
+++ ocr-id-tec/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './',
   css: {
     // 全局配置 utils.scs，详细配置参考 vue-cli 官网
     loaderOptions: {
