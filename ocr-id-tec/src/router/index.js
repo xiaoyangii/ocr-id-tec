2dacf9fc4a78fa719b67250e44ae1570c66ed0fa
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-06 20:33:37 +0800

    :large_blue_circle:  feat ： vue lazy-router

--- ocr-id-tec/src/router/index.js
+++ ocr-id-tec/src/router/index.js
@@ -1,13 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const home = () => import('@/views/layout/home.vue')
+const homepage = () => import('@/views/layout/homepage.vue')
+const retrievalengineindex = () => import('@/views/retrievalengine/index.vue')
+
+const bibliography = () => import(/* webpackChunkName: 'bibliography' */ '@/views/retrievalengine/bibliography/bibliography.vue')
+const articleDetails = () => import(/* webpackChunkName: 'bibliography' */ '@/views/retrievalengine/bibliography/articleDetails.vue')
+const score = () => import(/* webpackChunkName: 'bibliography' */ '@/views/retrievalengine/bibliography/score.vue')
+const articleOnline = () => import(/* webpackChunkName: 'bibliography' */ '@/views/retrievalengine/bibliography/articleOnline.vue')
+
+const bulkImport = () => import(/* webpackChunkName: 'bulkImport' */ '@/views/retrievalengine/bulkImport.vue')
+const bulkImportImg = () => import(/* webpackChunkName: 'bulkImport' */ '@/views/retrievalengine/bulkImportImg.vue')
+
+const historicalBrowsing = () => import('@/views/retrievalengine/historicalBrowsing.vue')
+const myDownload = () => import('@/views/retrievalengine/myDownload.vue')
+const myConcern = () => import('@/views/retrievalengine/myConcern.vue')
+const myCollection = () => import('@/views/retrievalengine/myCollection.vue')
+const mySubscription = () => import('@/views/retrievalengine/mySubscription.vue')
+const recyclebin = () => import('@/views/retrievalengine/recyclebin.vue')
+
+const onlinestore = () => import('@/views/onlinestore/index.vue')
+const smartcloud = () => import('@/views/smartcloud/index.vue')
+
+const feedback = () => import('@/views/layout/feedback/feedback.vue')
+const feedbackBefore = () => import('@/views/layout/feedback/feedbackBefore.vue')
+const feedbackAfter = () => import('@/views/layout/feedback/feedbackAfter.vue')
+
+const login = () => import(/* webpackChunkName: 'login' */ '@/views/login/index.vue')
+const passwordLogin = () => import(/* webpackChunkName: 'login' */ '@/views/login/passwordLogin.vue')
+const codeLogin = () => import(/* webpackChunkName: 'login' */ '@/views/login/codeLogin.vue')
+const registered = () => import(/* webpackChunkName: 'login' */ '@/views/login/registered.vue')
+const forgetPassword = () => import(/* webpackChunkName: 'login' */ '@/views/login/forgetPassword.vue')
+
+const personalCenter = () => import(/* webpackChunkName: 'personalCenter' */ '@/views/layout/personalCenter/personalCenter.vue')
+const personalData = () => import(/* webpackChunkName: 'personalCenter' */ '@/views/layout/personalCenter/personalData.vue')
+const homeData = () => import(/* webpackChunkName: 'personalCenter' */ '@/views/layout/personalCenter/homeData.vue')
+
+const settings = () => import('@/views/layout/settings.vue')
+
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/layout/home.vue'),
+    component: home,
     redirect: '/home/homepage',
     children: [
       // 首页
@@ -15,88 +53,88 @@ const routes = [
         path: '/home/homepage',
         name: 'Homepage',
         meta: { index: 0 },
-        component: () => import('@/views/layout/homepage.vue'),
+        component: homepage,
       },
       // 检索引擎
       {
         path: '/home/retrievalengine',
         name: 'retrievalengine',
         meta: { index: 1 },
-        component: () => import('@/views/retrievalengine/index.vue'),
+        component: retrievalengineindex,
         redirect: '/home/retrievalengine/bibliography',
         children: [
           // 检索引擎的文献文库
           {
             path: '/home/retrievalengine/bibliography',
             name: 'bibliography',
-            component: () => import('@/views/retrievalengine/bibliography/bibliography.vue')
+            component: bibliography
           },
           {
             path: '/home/retrievalengine/bibliography/:id',
             name: 'articleDetails',
-            component: () => import('@/views/retrievalengine/bibliography/articleDetails.vue'),
+            component: articleDetails,
             // 在路由配置文件中设置自定义的路由路径到meta.activeIndex属性中，来控制菜单自定义高亮显示
             meta: { activeIndex: '/home/retrievalengine/bibliography' }
           },
           {
             path: '/home/retrievalengine/bibliography/:id/score',
             name: 'score',
-            component: () => import('@/views/retrievalengine/bibliography/score.vue'),
+            component: score,
             meta: { activeIndex: '/home/retrievalengine/bibliography' }
           },
           {
             path: '/home/retrievalengine/bibliography/:id/online',
             name: 'articleOnline',
-            component: () => import('@/views/retrievalengine/bibliography/articleOnline.vue'),
+            component: articleOnline,
             meta: { activeIndex: '/home/retrievalengine/bibliography' }
           },
           // 检索引擎的批量导入
           {
             path: '/home/retrievalengine/bulkimport',
             name: 'bulkImport',
-            component: () => import('@/views/retrievalengine/bulkImport.vue')
+            component: bulkImport
           },
           {
             path: '/home/retrievalengine/bulkimportimg',
             name: 'bulkImportImg',
-            component: () => import('@/views/retrievalengine/bulkImportImg.vue'),
+            component: bulkImportImg,
             meta: { activeIndex: '/home/retrievalengine/bulkimport' }
           },
           // 检索引擎的历史记录
           {
             path: '/home/retrievalengine/historicalBrowsing',
             name: 'history',
-            component: () => import('@/views/retrievalengine/historicalBrowsing.vue')
+            component: historicalBrowsing
           },
           // 检索引擎的我的下载
           {
             path: '/home/retrievalengine/myDownload',
             name: 'myDownload',
-            component: () => import('@/views/retrievalengine/myDownload.vue')
+            component: myDownload
           },
           // 检索引擎的我的关注
           {
             path: '/home/retrievalengine/myconcern',
             name: 'myConcern',
-            component: () => import('@/views/retrievalengine/myConcern.vue')
+            component: myConcern
           },
           // 检索引擎的我的收藏
           {
             path: '/home/retrievalengine/mycollection',
             name: 'myCollection',
-            component: () => import('@/views/retrievalengine/myCollection.vue')
+            component: myCollection
           },
           // 检索引擎的我的订阅
           {
             path: '/home/retrievalengine/mysubscription',
             name: 'mySubscription',
-            component: () => import('@/views/retrievalengine/mySubscription.vue')
+            component: mySubscription
           },
           // 检索引擎回收站
           {
             path: '/home/retrievalengine/recyclebin',
             name: 'recyclebin',
-            component: () => import('@/views/retrievalengine/recyclebin.vue')
+            component: recyclebin
           },
         ]
       },
@@ -104,29 +142,29 @@ const routes = [
         path: '/home/onlinestore',
         name: 'onlinestore',
         meta: { index: 2 },
-        component: () => import('@/views/onlinestore/index.vue'),
+        component: onlinestore,
       },
       {
         path: '/home/smartcloud',
         name: 'smartcloud',
         meta: { index: 3 },
-        component: () => import('@/views/smartcloud/index.vue')
+        component: smartcloud
       },
       {
         path: '/home/feedback',
         name: 'feedback',
-        component: () => import('@/views/layout/feedback/feedback.vue'),
+        component: feedback,
         redirect: "/home/feedback/before",
         children: [
           {
             path: "/home/feedback/before",
             name: "beforeFeedback",
-            component: () => import('@/views/layout/feedback/feedbackBefore.vue'),
+            component: feedbackBefore
           },
           {
             path: "/home/feedback/after",
             name: "afterFeedback",
-            component: () => import('@/views/layout/feedback/feedbackAfter.vue'),
+            component: feedbackAfter
           },
         ]
       }
@@ -136,28 +174,28 @@ const routes = [
   {
     path: "/login",
     name: "login",
-    component: () => import("@/views/login/index.vue"),
+    component: login,
     redirect: "/passwordlogin",
     children: [
       {
         path: "/passwordlogin",
         name: "passwordLogin",
-        component: () => import("@/views/login/passwordLogin.vue"),
+        component: passwordLogin,
       },
       {
         path: "/codelogin",
         name: "codeLogin",
-        component: () => import("@/views/login/codeLogin.vue"),
+        component: codeLogin,
       },
       {
         path: "/registered",
         name: "registered",
-        component: () => import("@/views/login/registered.vue"),
+        component: registered,
       },
       {
         path: "/forgetpassword",
         name: "forgetPassword",
-        component: () => import("@/views/login/forgetPassword.vue"),
+        component: forgetPassword,
       },
     ]
   },
@@ -165,18 +203,18 @@ const routes = [
   {
     path: '/personalcenter',
     name: 'personalCenter',
-    component: () => import('@/views/layout/personalCenter/personalCenter.vue'),
+    component: personalCenter,
     redirect: '/personalcenter/personaldata',
     children: [
       {
         path: '/personalcenter/personaldata',
         name: 'personalData',
-        component: () => import('@/views/layout/personalCenter/personalData.vue'),
+        component: personalData,
       },
       {
         path: '/personalcenter/homedata',
         name: 'homeData',
-        component: () => import('@/views/layout/personalCenter/homeData.vue'),
+        component: homeData,
       }
     ]
   },
@@ -184,7 +222,7 @@ const routes = [
   {
     path: '/settings',
     name: 'settings',
-    component: () => import('@/views/layout/settings.vue'),
+    component: settings
   },
 ]
 
